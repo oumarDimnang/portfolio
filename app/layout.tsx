64f1738d8ec50b40b3f21ab91df265f176ee7cbe
Oumar Dimnang <oumar.dimnang@gmail.com>
2025-02-23 20:22:42 +0300

    changed font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,11 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Fira_Code } from "next/font/google";
 import Navbar from "@/components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
+const firaCode = Fira_Code({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "My Portfolio",
@@ -18,7 +20,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      <body className={inter.className}>
+      <body className={firaCode.className}>
         <Navbar />
         {children}
       </body>
